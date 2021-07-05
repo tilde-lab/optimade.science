e34846985446ed384dab9891a47598dc69deb924
Matvii Hodovaniuk <matvii@hodovani.uk>
2021-07-05 23:33:37 +0300

    add onClick function

--- src/views/Providers.svelte
+++ src/views/Providers.svelte
@@ -9,94 +9,12 @@
     {:then items}
         <Grid {items} {cols} style="justify-content: center;" let:item>
             <Popover pos="bottom">
-                <label slot="trigger">
+                <label slot="trigger" on:click={onClick}>
                     <input
-                        bind:group={$query.params.providers}
                         name="providers"
                         class={`provider-checkbox`}
                         id={item.id}
                         value={item.id}
-                        on:click={(event) => {
-                            let classList = event.target.classList;
-                            if (augmentation_mode) {
-                                if (
-                                    classList.contains('active') ||
-                                    classList.contains('exclusive')
-                                ) {
-                                    const all = document.getElementsByClassName(
-                                        'provider-checkbox'
-                                    );
-                                    $query.params.providers = [$query.params.providers];
-                                    for (let i = 0; i < all.length; i++) {
-                                        all[i].classList.remove('exclusive');
-                                        all[i].classList.add('active');
-
-                                        if (typeof $query.params.providers === 'string') {
-                                            $query.params.providers = [
-                                                $query.params.providers,
-                                                all[i].id,
-                                            ];
-                                        } else {
-                                            $query.params.providers.push(all[i].id);
-                                        }
-                                    }
-                                    augmentation_mode = false;
-                                } else {
-                                    const all = document.getElementsByClassName(
-                                        'provider-checkbox'
-                                    );
-                                    for (let i = 0; i < all.length; i++) {
-                                        all[i].classList.remove('exclusive');
-                                    }
-                                    event.target.classList.add('exclusive');
-                                    if (typeof $query.params.providers === 'string') {
-                                        $query.params.providers = [
-                                            $query.params.providers,
-                                            item.id,
-                                        ];
-                                    } else {
-                                        $query.params.providers.push(item.id);
-                                    }
-                                }
-                            } else {
-                                if (classList.contains('active')) {
-                                    event.target.classList.remove('active');
-                                    $query.params.providers = $query.params.providers.filter(
-                                        (id) => id !== item.id
-                                    );
-                                } else if (classList.contains('exclusive')) {
-                                    const all = document.getElementsByClassName(
-                                        'provider-checkbox'
-                                    );
-                                    $query.params.providers = [];
-                                    for (let i = 0; i < all.length; i++) {
-                                        all[i].classList.remove('exclusive');
-                                        all[i].classList.add('active');
-                                        if (
-                                            typeof $query.params.providers === 'string'
-                                        ) {
-                                            $query.params.providers = [
-                                                $query.params.providers,
-                                                all[i].id,
-                                            ];
-                                        } else {
-                                            $query.params.providers.push(all[i].id);
-                                        }
-                                    }
-                                } else {
-                                    let all = document.getElementsByClassName(
-                                        'provider-checkbox'
-                                    );
-                                    for (let i = 0; i < all.length; i++) {
-                                        all[i].classList.remove('exclusive');
-                                        all[i].classList.remove('active');
-                                    }
-                                    event.target.classList.add('exclusive');
-                                    $query.params.providers = [item.id];
-                                    augmentation_mode = true;
-                                }
-                            }
-                        }}
                         disabled={!item.attributes.base_url}
                         type="checkbox"
                     />
@@ -144,10 +62,25 @@
     const height: number = SIZE[size];
     const radius: number = height / 2;
     let augmentation_mode = false;
+
+    
 </script>
 
 <script lang="ts">
     let width: number = 0;
+    // const providersList =  $providers;
+
+    async function onClick(e){
+        const id = e.target.id;
+        if(!id) return;
+        // const p = await $providers;
+        if($query.params.providers.includes(e.target.id)){
+            $query.params.providers = $query.params.providers.filter(p => p !== id);
+        } else {
+            $query.params.providers = [...$query.params.providers, id]
+        }
+        console.log($query.params.providers);
+    }
 </script>
 
 <style>
