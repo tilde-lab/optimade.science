239f4e0931078cb3966c8d94e0efdd0e1b763899
Valexr <valexr@gmail.com>
2021-12-04 13:17:38 +0300

    remove commented code

--- src/views/Paginator.svelte
+++ src/views/Paginator.svelte
@@ -14,13 +14,11 @@
 <script lang="ts" context="module">
     import { query } from 'svelte-pathfinder';
     import { Pagination } from 'svelte-spectre';
-    import { getTotal, total } from '@/stores/search';
+    import { total } from '@/stores/search';
     import Section from '@/layouts/Section.svelte';
 </script>
 
 <script lang="ts">
-    // let total = 0;
-    $: console.log($total);
     $query.params.page = 1;
     $query.params.limit = 10;
 
@@ -28,6 +26,4 @@
         $query.params.page = 1;
         $query.params.limit = 10;
     }
-    // $: ($query.params.filter || $query.params.providers) &&
-    // $getTotal.then((t) => (total = t));
 </script>
