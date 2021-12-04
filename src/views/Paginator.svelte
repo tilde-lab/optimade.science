4b6a36a6324502875b685b9e47f0d8a50cb51e1b
Valexr <valexr@gmail.com>
2021-12-04 12:19:56 +0300

    inbind total

--- src/views/Paginator.svelte
+++ src/views/Paginator.svelte
@@ -4,7 +4,7 @@
             <Pagination
                 bind:limit={$query.params.limit}
                 bind:page={$query.params.page}
-                bind:total
+                {total}
                 rest={7}
             />
         </Section>
@@ -14,7 +14,7 @@
 <script lang="ts" context="module">
     import { query } from 'svelte-pathfinder';
     import { Pagination } from 'svelte-spectre';
-    import { getTotal, searchAll } from '@/stores/search';
+    import { getTotal } from '@/stores/search';
     import Section from '@/layouts/Section.svelte';
 </script>
 
@@ -24,37 +24,10 @@
     $query.params.page = 1;
     $query.params.limit = 10;
 
-    $: if ($query.params.filter && $query.params.providers) {
-    } else {
+    $: if (!$query.params.filter && !$query.params.providers) {
         $query.params.page = 1;
         $query.params.limit = 10;
     }
     $: ($query.params.filter || $query.params.providers) &&
         $getTotal.then((t) => (total = t));
-
-    // async function getTotal(searchAll) {
-    //     const fetchedProviders = await searchAll;
-    //     const filteredProviders = fetchedProviders.filter(
-    //         ([apis, provider]) =>
-    //             apis &&
-    //             apis.some((a) => !(a instanceof Error) && a?.data.length)
-    //     );
-    //     const returnedTotals = filteredProviders.reduce(
-    //         (acc, [apis, provider]) => {
-    //             const returned =
-    //                 apis[0].meta.data_returned /
-    //                 (apis[0].meta.data_returned > 10000 ? 100 : 1);
-    //             acc = acc.length ? [...acc, returned] : [returned];
-    //             return acc;
-    //         },
-    //         []
-    //     );
-    //     console.log(
-    //         filteredProviders,
-    //         returnedTotals,
-    //         $query.params.filter,
-    //         $query.params.providers
-    //     );
-    //     return returnedTotals.length ? Math.max(...returnedTotals) : 0;
-    // }
 </script>
