8519a04c68ffbe2957ec2bcd6a76c8a570aaf4ba
Valexr <valexr@gmail.com>
2021-12-09 18:06:31 +0300

    updeps & remove undeed code

--- src/services/optimade.ts
+++ src/services/optimade.ts
@@ -6,13 +6,9 @@ import { providersUrl, corsProxyUrl } from '@/config';
 
 const optimade = new Optimade({ providersUrl, corsProxyUrl, });
 
-// Add an API version to a provider. We use it in Avatar.svelte.
-prefetched.providers && Object.keys(prefetched.providers).forEach((key) => {
-    prefetched.providers[key].attributes.api_version = prefetched.apis[key][0].attributes.api_version;
-});
-
 optimade.providers = prefetched.providers;
 optimade.apis = prefetched.apis;
+
 
 export default optimade;
 
