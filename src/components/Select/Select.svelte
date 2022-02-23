ff0d739498a44c428caead4baa965a54d99d206a
Valexr <valexr@gmail.com>
2022-02-23 20:26:51 +0300

    svelte-select darkTheme

--- src/components/Select/Select.svelte
+++ src/components/Select/Select.svelte
@@ -11,7 +11,7 @@
     export let selectedValue;
 </script>
 
-<style>
+<style lang="scss">
     div {
         display: contents;
         --border: 0.05rem solid #bcc3ce;
@@ -23,5 +23,27 @@
         --placeholderColor: #bcc3ce;
         --indicatorTop: 8px;
         --clearSelectTop: 8px;
+        --listBackground: #f1f1fc;
+    }
+    @media (prefers-color-scheme: dark) {
+        div {
+            --background: #252b33;
+            --inputColor: #f2f2f2;
+            --listBackground: #343a51;
+        }
+    }
+    :global([color-scheme='dark']) {
+        div {
+            --background: #252b33;
+            --inputColor: #f2f2f2;
+            --listBackground: #343a51;
+        }
+    }
+    :global([color-scheme='light']) {
+        div {
+            --background: #fff;
+            --inputColor: #3b4351;
+            --listBackground: #f1f1fc;
+        }
     }
 </style>
