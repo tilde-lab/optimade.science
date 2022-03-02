b1a1f9c73f8ca5a7effe32f016985e3e23e5178a
Valexr <valexr@gmail.com>
2022-03-02 10:34:52 +0300

    fix .jsonView colors

--- src/components/JSON/Viewer.svelte
+++ src/components/JSON/Viewer.svelte
@@ -32,5 +32,43 @@
 <style lang="scss">
     :global(.jsonView) {
         user-select: text;
+        color: #3b4351;
+        :global(.name) {
+            color: #5755d9;
+        }
+        :global(.value.null),
+        :global(.value.undefined) {
+            color: #bcc3ce;
+        }
+        :global(.value.boolean),
+        :global(.value.string),
+        :global(.value.number) {
+            color: #d73e48;
+        }
+        :global(.name:hover),
+        :global(.value:hover) {
+            background-color: #ffe9b3;
+        }
+    }
+
+    @media (prefers-color-scheme: dark) {
+        :global(.jsonView) {
+            color: #bcc3ce;
+            :global(.value.null),
+            :global(.value.undefined) {
+                color: #3b4351;
+            }
+        }
+    }
+    :global([color-scheme='dark']) {
+        :global(.jsonView) {
+            color: #bcc3ce;
+            :global(.value.null),
+            :global(.value.undefined) {
+                color: #3b4351;
+            }
+        }
+    }
+    :global([color-scheme='light']) {
     }
 </style>
