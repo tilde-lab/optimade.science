8d28e426d0018a6fe28d2fdc4644dc0841be390e
Valexr <valexr@gmail.com>
2022-05-26 19:34:37 +0300

    updeps & replce Autocomplete by Select

--- src/stores/modules.ts
+++ src/stores/modules.ts
@@ -3,10 +3,10 @@ import { asyncable, syncable } from 'svelte-asyncable';
 import type { Readable } from 'svelte/store';
 import type { Asyncable } from 'svelte-asyncable';
 
-import { getJSON } from '@/services/optimade';
+import { getJSON } from '@/services/optimade'; // now replaced by simple fetch() for ignore CORS issue
 import { supportedModulesUrl, lsModulesKey } from '@/config';
 
-export const builtinModules: Asyncable<string[]> = asyncable(() => getJSON(supportedModulesUrl), null);
+export const builtinModules: Asyncable<string[]> = asyncable(() => fetch(supportedModulesUrl).then(async (res) => await res.json()), null);
 
 export const builtinModulesSync: Readable<string[]> = syncable(builtinModules, []);
 
--- src/views/ModuleSelect/ModuleSelect.svelte
+++ src/views/ModuleSelect/ModuleSelect.svelte
@@ -1,30 +1,12 @@
-{#await $modules then predefined}
-	<Autocomplete {groupBy} {predefined} bind:selected creatable placeholder="Select app or enter app URL" empty="No apps added" />
+{#await $modules then options}
+	<Select {options} bind:value={selectedValue} placeholder="Select app or enter app URL" style="margin: 0 0 1em" />
 {/await}
 
 <script lang="ts" context="module">
-	import { Autocomplete } from 'svelte-spectre';
-
-	import modules, { builtinModulesSync } from '@/stores/modules';
-
-	import { moduleGroups } from '@/config';
+	import { Select } from 'svelte-spectre';
+	import modules from '@/stores/modules';
 </script>
 
 <script lang="ts">
-	let selected: any[] = [];
-
 	export let selectedValue: string;
-
-	$: selectedValue = selected[0]?.value;
-
-	$: if (selectedValue) {
-		modules.update(async ($modules) => {
-			if (!(await $modules).includes(selectedValue)) {
-				(await $modules).unshift(selectedValue);
-			}
-			return $modules;
-		});
-	}
-
-	const groupBy = (item: { value: string }): string => ($builtinModulesSync.includes(item.value) ? moduleGroups.builtin : moduleGroups.local);
 </script>
