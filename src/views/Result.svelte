b023ff5d9adcc68f1d5a534746c8e74024234f70
Evgeny Blokhin <eb@tilde.pro>
2025-11-04 20:10:54 +0100

    Add integration WIP

--- src/views/Result.svelte
+++ src/views/Result.svelte
@@ -6,7 +6,7 @@
 		<Col md="12">
 			{#if data}
 				<Input bind:value={filter} placeholder="Field name..." />
-				<small class="form-text text-muted"> Filter data object by field name. </small>
+				<small class="form-text text-muted">filter object by field name</small>
 				<JSON.Viewer value={data} caption="Optimade" {filter} />
 			{:else}
 				<JSON.Editor style="width: 100%; height: 400px; margin-top: 0;" on:change={(e) => (code = e.detail)} />
@@ -17,13 +17,14 @@
 			<ModuleSelect bind:selectedValue={module} />
 			<br />
 			<div class="module">
-				<iframe name="visualisationFrame" bind:this={iframe} src={module} title="App window" frameborder=0 scrolling="no" />
+				<iframe name="visualisationFrame" bind:this={iframe} src={module} frameborder=0 scrolling="no" />
 			</div>
 		</Col>
 	</Grid>
 </div>
 
 <script lang="ts" context="module">
+	import { onMount } from 'svelte';
 	import { Col, Divider, Grid, Hero, Input, Steps } from 'svelte-spectre';
 
 	import ModuleSelect from '@/views/ModuleSelect/ModuleSelect.svelte';
@@ -36,8 +37,8 @@
 
 <script lang="ts">
 	export let data: Record<string, never> | null | undefined = null;
-
 	let code: string;
+
 	let module: any;
 	let filter = '';
 	let iframe: HTMLIFrameElement | null;
@@ -45,6 +46,9 @@
 	$: if (iframe) {
 		const message = data || code;
 		iframe.onload = function () {
+
+			if (!message) return;
+
 			// We wait untill we get a link with a visualisation file
 			const timer = setInterval(() => {
 				const visualisationFrame = window.frames.visualisationFrame;
@@ -57,6 +61,16 @@
 			}, 500);
 		};
 	}
+
+	onMount(() => {
+		window.addEventListener('message', (message) => {
+			if (message.data === 'need-absolidix-login'){
+				console.log(message.data);
+			} else if (message.data === 'absolidix-ok'){
+				console.log(message.data);
+			}
+		});
+	});
 </script>
 
 <style>
